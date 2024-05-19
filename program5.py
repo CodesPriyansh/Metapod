92e762c162abcb3752bfdb844b7a7d9f61858d8e
Codes Priyansh <codespriyansh@gmail.com>
2024-05-19 13:32:12 +0530

    Add files via upload

--- program5.py
+++ program5.py
@@ -3,22 +3,49 @@
 import random 
 
 #write answers
+ans1 = "work hard."
+ans2 = "learn skills."
+ans3 = "find love."
+ans4 = "hug your father."
+ans5 = "retire your mother."
+ans6 = "become strong."
+ans7 = "die with no regrets."
+ans8 = "write poetry." 
 
-ans1 = "Go for it!"
-ans2 = "No way, Jose!"
-ans3 = "I'm not sure. Ask me again."
-ans4 = "Fear of the unknown is what imprisons us."
-ans5 = "It would be madness to do that."
-ans6 = "Only you can save mankind"
-ans7 = "Makes no difference to me, do or don't - whatever."
-ans8 = "Yes, I think on balance that is the right choice."
+print("WelcomeToMyMagic8Ball.") 
 
-print("Welcome to MyMagic8Ball.")
-
+#get the users name 
+user_name = input("what's your name: \n")
 #get the users question
+question = input("Ask me for advice then press enter to shake me:\n") 
+print("shaking..." * 4)
 
-question = input("Ask me for advice then press RETURN to shake me. \n")
-print("shaking..." * 4) 
+# use the randint() function to select the correct answer.
+
+choice = random.randint(1, 8) 
+
+if choice == 1:
+    answer = ans1
+elif choice == 2:
+    answer = ans2
+elif choice == 3:
+    answer = ans3
+elif choice == 4:
+    answer = ans4 
+elif choice== 5:
+    answer = ans5
+elif choice == 6:
+    answer = ans6 
+elif choice == 7:
+    answer = ans7
+else:
+    answer = ans8
+    
+#print the answer to the screen
+print(answer) 
+
+input("press the enter to finish: \n") 
+print("thanks for playing", user_name)
 
 
 
